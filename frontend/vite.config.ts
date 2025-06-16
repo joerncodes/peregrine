80df79d48d939f2de604455ab4279ccd1d49bf6a
Jörn Meyer <joern.meyer@kernpunkt.de>
2025-06-16 14:03:29 +0200

    load backend URI

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,10 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { loadEnv } from "vite";
 
+const env = loadEnv('development', process.cwd(), '');
+const apiUrl = env.VITE_API_URL;
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -14,12 +17,12 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3001",
+        target: apiUrl,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
       "/images": {
-        target: "http://localhost:3001",
+        target: apiUrl,
         changeOrigin: true,
       },
     },
